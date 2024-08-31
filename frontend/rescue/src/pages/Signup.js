f1e7a391d1432e245ed8f516cf3a5a8e8514623f
SurajSenapati24 <137797757+SurajSenapati24@users.noreply.github.com>
2024-08-31 12:05:04 +0530

    Update Signup.js

--- frontend/rescue/src/pages/Signup.js
+++ frontend/rescue/src/pages/Signup.js
@@ -1,96 +1,96 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Link, useNavigate } from 'react-router-dom'
-// import { set } from 'mongoose'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import './Signup.css';  // Import the CSS file
 
 function SignUp() {
-    // const [users, setUsers] = useState([])
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const navigate = useNavigate();
-
 
     useEffect(() => {
         fetchUsers();
-    }, [])
+    }, []);
 
-    const fetchUsers = () => {
-        axios
-        .get('http://localhost:3001/signup')
-        .then((res) => {
-            // console.log(res.data)
-        })
-    }
-
+    const fetchUsers = async () => {
+        await axios
+            .get('http://localhost:3001/signup')
+            .then((res) => {
+                // Handle response
+            });
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
         axios
-        .post('http://localhost:3001/signup', { name, email, password })
-        .then(() => {
-            alert('Registration Successful')
-            setName('')
-            setEmail('')
-            setPassword('')
-            fetchUsers();
-            navigate('/login')
-        })
-        .catch((error) => {
-            console.log('Unable to register user')
-        })
+            .post('http://localhost:3001/signup', { name, email, password })
+            .then(() => {
+                alert('Registration Successful');
+                setName('');
+                setEmail('');
+                setPassword('');
+                fetchUsers();
+                navigate('/login');
+            })
+            .catch((error) => {
+                console.log('Unable to register user',error);
+                alert('Registeration failed. Please try again.')
+            });
+    };
 
-    }
-
-  return (
-    <div>
-        <div>
-            <form 
-            onSubmit={handleSubmit}>
-                {/* Email Input */}
-                <label>Name</label>
-                <br />
-                <input
-                type='text'
-                placeholder='Name'
-                value={name}
-                onChange={(e) => setName(e.target.value)} />
-                <br />
-                <br />
-                 {/*Username Input */}
-                 <label>Email</label>
-                <br />
-                <input
-                type='text'
-                placeholder='Email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)} />
-                <br />
-                <br />
-                 {/* Password Input */}
-                 <label>Password</label>
-                <br />
-                <input 
-                type='password'
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)} />
-                <br />
-                <br />
-                {/* Button */}
-                <button
-                type='submit'>Sign Up</button>
-                <p>You agree to our terms and policies.</p>
-                    <Link
-                        to="/login"
-                        style={{ borderColor: '#fbc02d', color: '#333', fontWeight: 'bold', transition: 'background-color 0.3s ease, color 0.3s ease' }}
-                    >   
+    return (
+        <div className="signup-container">
+            <div className="signup-box">
+                <form onSubmit={handleSubmit}>
+                    {/* Name Input */}
+                    <label className="signup-label">Name</label>
+                    <br />
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="signup-input"
+                    />
+                    <br />
+                    <br />
+                    {/* Email Input */}
+                    <label className="signup-label">Email</label>
+                    <br />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="signup-input"
+                    />
+                    <br />
+                    <br />
+                    {/* Password Input */}
+                    <label className="signup-label">Password</label>
+                    <br />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="signup-input"
+                    />
+                    <br />
+                    <br />
+                    {/* Sign Up Button */}
+                    <button type="submit" className="signup-button">
+                        Sign Up
+                    </button>
+                    <p>You agree to our terms and policies.</p>
+                    <Link to="/login" className="signup-link">
                         Login
                     </Link>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default SignUp
+export default SignUp;
